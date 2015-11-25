4bec566fe8247c528a6225121c86042de62f0e71
Jarnehall <alexander@jarnehall.se>
2015-11-25 01:47:54 +0100

    coded header with nav and landing section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-$(function (){
+$("#menuIcon").click(function() {
+  $("#head").toggleClass("active");
+  $("#nav").toggleClass("active");
+});
 
-
-	
-
-
+$(window).scroll(function() {
+	if($(this).scrollTop()>=20){
+	  $("#head").toggleClass("scrolled", true);
+	}
+	else {
+	  $("#head").toggleClass("scrolled", false);
+	}
 });
